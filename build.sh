6ea361fd2bdec3c34f3ffbc05ec6427ac90c51c2
Jeffrey Paine <jeffrey.e.paine.th@dartmouth.edu>
2025-11-19 12:49:24 +0800

    Add debug output to build script

--- build.sh
+++ build.sh
@@ -9,6 +9,10 @@ tar -xzf hugo.tar.gz
 chmod +x hugo
 export PATH="$PWD:$PATH"
 
-# Build the site
-./hugo --gc --minify
+# Debug: Check theme structure
+echo "Checking theme structure..."
+ls -la themes/shibui/layouts/_partials/ || echo "Theme partials not found"
+
+# Build the site with verbose output
+./hugo --gc --minify --verbose
 
